05d9d2e7650dd0a4ac2a6f7d542570eab437b4bb
nggezi <155415081+nggezi@users.noreply.github.com>
2025-09-24 15:52:30 +0800

    Update fofa.py

--- fofa.py
+++ fofa.py
@@ -3,7 +3,8 @@
 # @Time    : 2022/9/14 22:20
 # @Author  : Cl0udG0d
 # @File    : fofa.py
-# @Github: https://github.com/Cl0udG0d
+# @Github  : https://github.com/Cl0udG0d
+
 import argparse
 import os
 import sys
@@ -13,27 +14,20 @@ from tookit import unit, config
 from tookit.levelData import LevelData
 from tookit.outputData import OutputData
 from tookit.unit import clipKeyWord, outputLogo
-import gettext
-import locale
 import mmh3
 import requests
 import base64
 import codecs
+
+# 设置项目根路径
 config.ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+
 if getattr(sys, 'frozen', None):
     dir = sys._MEIPASS
 else:
     dir = config.ROOT_PATH
-# 获取当前的语言设置
-# lang, _ = locale.getdefaultlocale()
-# if lang and lang.startswith('zh'):
-    # 如果是中文环境，不需要翻译，直接用原始字符串
-#    _ = lambda x: x
-# else:
-    # 如果是其他语言环境，则加载对应的翻译文件
-#    language = gettext.translation('fofa_hack', localedir=os.path.join(dir, "locale"), languages=['en'])
-#    language.install()
-#    _ = language.gettext
+
+# 注释掉翻译相关代码，直接使用原始字符串
 _ = lambda x: x
 
 def main():
@@ -54,41 +48,40 @@ def main():
     parser.add_argument('--outputname','-on', help=_("指定输出文件名，默认文件名为 fofaHack"))
     parser.add_argument('--fuzz', '-f', help=_('关键字fuzz参数,增加内容获取粒度'), action='store_true')
 
-    parser.add_argument('--proxy-type',choices=['socks4','socks5', 'http'], help=_("代理类型,默认为http"),default='http')
+    parser.add_argument('--proxy-type', choices=['socks4','socks5', 'http'], help=_("代理类型,默认为http"), default='http')
     parser.add_argument('--authorization', type=str, help="指定Authorization值")
     parser.add_argument('--authorization-file', type=str, help="从文件中读取authorization列表 --authorization-file authorization.txt")
+    parser.add_argument('--fofa-key', type=str, help="fofa api key值(配合fofa终身会员使用)")
+    parser.add_argument('--debug', help="fofa-hack调试模式,运行过程中输出更多运行日志", action='store_true')
+    parser.add_argument('--time-type', help="fofa-hack时间类型 (day | hour)，默认为day，如果选择hour的话数据采集粒度会变成按小时的", choices=['day','hour'], default='day')
 
-    parser.add_argument('--fofa-key', type=str,
-                        help="fofa api key值(配合fofa终身会员使用)")
-    parser.add_argument('--debug',
-                        help="fofa-hack调试模式,运行过程中输出更多运行日志", action='store_true')
-    parser.add_argument('--time-type',
-                        help="fofa-hack时间类型 (day | hour)，默认为day，如果选择hour的话数据采集粒度会变成按小时的",choices=['day','hour'],default='day')
     proxy_group = parser.add_mutually_exclusive_group()
     proxy_group.add_argument('--proxy', help=_("指定代理,代理格式 --proxy '127.0.0.1:7890'"))
     proxy_group.add_argument('--proxy-url', help=_("指定代理url，即访问URL响应为proxy,代理格式 --proxy-url http://127.0.0.1/proxy_pool/get"))
     proxy_group.add_argument('--proxy-file', help=_("指定txt格式的代理文件,按行分割,代理格式 --proxy-file proxy.txt"))
-    # parser.add_argument('--type', type=str, choices=["common", "selenium"], default="common",
-    #                     help="运行类型,默认为普通方式")
+
     args = parser.parse_args()
 
+    # 调试模式
     if args.debug:
         config.DEBUG = args.debug
-
     if args.time_type:
         config.TIME_TYPE = args.time_type
-
     if args.fofa_key:
         config.FOFA_KEY = args.fofa_key
 
+    # 读取 authorization 文件
     if args.authorization_file:
         config.AUTHORIZATION_FILE = args.authorization_file
         with open(config.AUTHORIZATION_FILE, 'r') as f:
             for line in f.readlines():
-                config.AUTHORIZATION_LIST.append(line)
+                config.AUTHORIZATION_LIST.append(line.strip())
 
     time_sleep = int(args.timesleep)
     timeout = int(args.timeout)
+
+    # 处理搜索关键字
+    search_key = ""
     if args.keyword:
         search_key = clipKeyWord(args.keyword)
     elif args.base:
@@ -96,17 +89,13 @@ def main():
             search_key = base64.b64decode(args.base).decode('utf-8')
         except Exception as e:
             print(e)
-            search_key = ""
-            pass
     elif args.iconurl:
-        if args.iconurl.endswith(".ico"):
-            icon_url = args.iconurl
-        else :
-            icon_url = args.iconurl + "favicon32.ico" if  args.iconurl.endswith("/") else args.iconurl + "/favicon32.ico"
+        icon_url = args.iconurl
+        if not icon_url.endswith(".ico"):
+            icon_url = icon_url.rstrip("/") + "/favicon32.ico"
         try:
-            _icon = mmh3.hash(
-                codecs.lookup('base64').encode(requests.get(icon_url).content)[0])
-            search_key = '''icon_hash="{}"'''.format(_icon)
+            _icon = mmh3.hash(codecs.lookup('base64').encode(requests.get(icon_url).content)[0])
+            search_key = f'''icon_hash="{_icon}"'''
         except:
             print("icon url " + icon_url + " 访问错误")
             exit(0)
@@ -115,41 +104,32 @@ def main():
         if os.path.exists(file_path):
             with open(file_path, 'rb') as f:
                 _icon = mmh3.hash(codecs.lookup('base64').encode(f.read())[0])
-                search_key = '''icon_hash="{}"'''.format(_icon)
-    else:
-        search_key = ""
+                search_key = f'''icon_hash="{_icon}"'''
 
     endcount = int(args.endcount) if args.endcount else 100
     level = args.level if args.level else "1"
     level_data = LevelData(level)
     fuzz = args.fuzz
-
-
     output = args.output if args.output else "txt"
     outputname = args.outputname if args.outputname else "fofaHack"
 
     if search_key:
-        # if outputname:
-        filename="{}.{}".format(outputname,output)
-        # 检查文件是否存在
+        filename="{}.{}".format(outputname, output)
         if os.path.exists(filename) and os.path.exists("final_"+filename):
-            # 如果存在，删除文件
             os.remove(filename)
             os.remove("final_"+filename)
-        # else:
-        #     filename = "{}_{}.{}".format(unit.md5(search_key), int(time.time()), output)
         output_data = OutputData(filename, level, pattern=output)
     else:
-        # filename = _("暂无")
-        filename="{}.{}".format(outputname,output)
+        filename="{}.{}".format(outputname, output)
         output_data = OutputData(filename, level, pattern=output)
 
-    if args.proxy or args.proxy_url or args.proxy_file :
+    # 配置代理
+    if args.proxy or args.proxy_url or args.proxy_file:
         config.IS_PROXY = True
         if args.proxy:
             config.PROXY_SINGLE = True
             config.PROXY_ARGS = args.proxy
-        elif args.proxy_url :
+        elif args.proxy_url:
             config.PROXY_FROM_URL = True
             config.PROXY_ARGS = args.proxy_url
         else:
@@ -158,21 +138,17 @@ def main():
     if args.proxy_type:
         config.PROXY_TYPE = args.proxy_type
 
-    # is_proxy, proxy = setProxy(args.proxy,args.proxy_type)
-    # type = args.type
     inputfile = args.inputfile if args.inputfile else None
     if not inputfile and not search_key:
         print(_("未输入搜索内容"))
         exit(0)
+
     if args.authorization:
-        # 用户输入了Authorization值
-        # print("用户输入的Authorization值为:", args.authorization)
         config.AUTHORIZATION = args.authorization
 
-    fofa = FofaMain(search_key, inputfile, filename, time_sleep, endcount, level, level_data, output, output_data,
-                    fuzz, timeout, config.IS_PROXY)
+    fofa = FofaMain(search_key, inputfile, filename, time_sleep, endcount, level, level_data,
+                    output, output_data, fuzz, timeout, config.IS_PROXY)
     fofa.start()
-
 
 if __name__ == '__main__':
     main()
